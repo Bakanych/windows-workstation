35d213886b358de856cc240fc85f4ce76c035ddd
Bakanych <manych@gmail.com>
2022-12-20 19:01:37 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,4 @@
-Import-Module posh-git
-
-oh-my-posh init --config "$PSScriptRoot/theme.omp.json" pwsh | Invoke-Expression
-
+oh-my-posh init --config "$env:USERPROFILE/theme.omp.json" pwsh | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true
 
 function Get-PersistentHistory {
@@ -12,6 +9,12 @@ function Start-Compose {
     Invoke-Expression "docker compose $($args -join ' ')"
 }
 
+function Write-ProcessDetails{
+    $args = $args[0] ?? '*'
+    clear; while (1) {ps "$args" | Format-Table; sleep 1;clear}
+}
+
 Set-Alias -Name history -Value Get-PersistentHistory -Option AllScope
+Set-Alias -Name top -Value Write-ProcessDetails
 Set-Alias -Name grep -Value Select-String
 Set-Alias -Name dc -Value Start-Compose
